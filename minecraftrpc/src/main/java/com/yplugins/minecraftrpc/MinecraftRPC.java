e5d6ffc770a89dc6f1674b845ab352e0e8beb787
Rivo <rivo.tuksammel@gmail.com>
2025-05-12 10:51:08 +0300

    Refactor code structure for improved readability and maintainability

---
+++ minecraftrpc/src/main/java/com/yplugins/minecraftrpc/MinecraftRPC.java
@@ -0,0 +1,69 @@
+package com.yplugins.minecraftrpc;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.net.InetSocketAddress;
+import org.bukkit.Bukkit;
+
+import io.grpc.Server;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftRPC extends JavaPlugin {
+
+    private static final Logger logger = Bukkit.getLogger();
+    private Server grpcServer = null;
+
+    // This code is called after the server starts and after the /reload command
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        logger.log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
+        saveDefaultConfig(); // Save the default config.yml if it doesn't exist.
+
+        var port = getConfig().getInt("port");
+        var host = getConfig().getString("host");
+
+        if (port <= 0 || port > 65535) {
+            logger.log(Level.SEVERE, "Invalid port number: {0}", port);
+            return;
+        }
+
+        if (host == null || host.isEmpty()) {
+            logger.log(Level.SEVERE, "Invalid host: {0}", host);
+            return;
+        }
+
+        logger.log(Level.INFO, String.format("Starting gRPC server on {0}:{1}") , new Object[]{host, port});
+        var grpcAddress = new InetSocketAddress(host, port);
+
+        var grpcServerBuilder = NettyServerBuilder
+            .forAddress(grpcAddress)
+            .addService(new CommandService(this));
+
+        this.grpcServer = grpcServerBuilder.build();
+
+        try {
+            this.grpcServer.start();
+            logger.log(Level.INFO, "gRPC server started on {0}:{1}", new Object[]{host, port});
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Failed to start gRPC server: {0}", e.getMessage());
+            return;
+        }
+    }
+
+    // This code is called before the server stops and after the /reload command
+    @Override
+    public void onDisable() {
+        logger.log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
+        if (this.grpcServer != null) {
+            try {
+                this.grpcServer.shutdown();
+                this.grpcServer.awaitTermination();
+                logger.log(Level.INFO, "gRPC server stopped");
+            } catch (InterruptedException e) {
+                logger.log(Level.SEVERE, "Failed to stop gRPC server: {0}", e.getMessage());
+            }
+        }
+    }
+}
